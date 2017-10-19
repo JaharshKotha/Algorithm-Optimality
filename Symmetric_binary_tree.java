e32bf65cc1cc16d4d7b00a73f85aef46ad2b7447
Jaharsh Samayan <jaharsh93@gmail.com>
2017-10-19 14:25:35 -0700

    Update Symmetric_binary_tree.java

--- Symmetric_binary_tree.java
+++ Symmetric_binary_tree.java
@@ -1,29 +1,21 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        
+class Tree {
+    public boolean checkSymmetric(TreeNode root) {
+        //An helper function that keeps track of my left and right subtrees.
         return help( root,  root);
     }
     
     public static boolean help(TreeNode left,TreeNode right)
     {
+        //Base case when we reach end of tree
         if(left == right && left == null)
             return true;
         else if(left == null || right == null)
             return false;
         
-        
+        // Check for non symmetry at every point
         if(left.val != right.val)
             return false;
-        
+        // Keep check the reflection of the left and right tree at the same time
        return  help(left.left,right.right) && 
         help(left.right,right.left);
         
